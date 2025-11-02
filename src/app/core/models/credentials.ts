222681c23c333d77a3780548fa9b31e31f266f31
Paco16889 <francisco.cobsan@gmail.com>
2025-11-02 17:27:54 +0100

    añadidas interfaces

--- src/app/core/models/credentials.ts
+++ src/app/core/models/credentials.ts
@@ -2,3 +2,14 @@ export interface Credentials{
     email:string;
     password:string;
 }
+
+export interface LoginResponse{
+    
+}
+export interface RegisterInfo{
+    name:string,
+    surname:string,
+    email:string,
+    password:string,
+    confirmPassword:string
+}
